d9962cd31ed4328b5321f0d8febf3c9d88299bbc
Rodrigo Feitosa Soares <rodrigo.soares@banese.com.br>
2018-09-19 15:39:49 -0300

    Remoção de código desnecessário

--- sandbox/src/br/com/rod/sandbox/binary/XOR.java
+++ sandbox/src/br/com/rod/sandbox/binary/XOR.java
@@ -30,27 +30,5 @@ public class XOR {
         int y = g ^ x; //lê-se 10 XOR 11, resultando em 01, que em decimal é 1
         System.out.println("y = g ^ x => "+ g +" ^ "+ x +" => "+ intToBinary(g,BINARY_PAD) +" XOR "+ intToBinary(x,BINARY_PAD) +" => "+ intToBinary(y,BINARY_PAD) +" => "+ y);
         System.out.println("------------------------------------------");
-        
-        
-        System.out.println("\nDeslocamento de Bit X Produto");
-        System.out.println("------------------------------------------");
-        int n = 80;
-        int div2 = n >> 1;
-        int div4 = n >> 2;
-        int div8 = n >> 3;
-        int mul2 = n << 1;
-        int mul4 = n << 2;
-        int mul8 = n << 3;
-        
-        System.out.println("n = " + n + " => " + intToBinary(n, BINARY_PAD));
-        System.out.println("------------------------------------------");
-        System.out.println("n >> 1 = "+ div2 +" <=> n / 2 = "+ n / 2);
-        System.out.println("n >> 2 = "+ div4  +" <=> n / 4 = "+ n / 4);
-        System.out.println("n >> 3 = "+ div8  +" <=> n / 8 = "+ n / 8);
-        System.out.println("------------------------------------------");
-        System.out.println("n << 1 = "+ mul2 +" <=> n * 2 = "+ n * 2);
-        System.out.println("n << 2 = "+ mul4  +" <=> n * 4 = "+ n * 4);
-        System.out.println("n << 3 = "+ mul8  +" <=> n * 8 = "+ n * 8);
-        System.out.println("------------------------------------------");
     }
 }
